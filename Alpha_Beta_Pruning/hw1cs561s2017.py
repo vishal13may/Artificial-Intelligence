07025dcda051a8c5d88d613664a2b3b1c9b8a6bc
vishal13may <vishal13may@gmail.com>
2017-02-02 01:00:58 -0800

    Second Level Moves

--- Alpha_Beta_Pruning/hw1cs561s2017.py
+++ Alpha_Beta_Pruning/hw1cs561s2017.py
@@ -8,6 +8,7 @@ class Othello:
 
 def find_valid_moves(start_player,board_position):
     validMoves = []
+    vMoves = []
     dic = {}
     dic[0] = 'a'
     dic[1] = 'b'
@@ -26,9 +27,10 @@ def find_valid_moves(start_player,board_position):
             if result:
                 move = ''
                 move = dic[column] + str(row+1)
-                validMoves.append(move)
-    print validMoves
-    return
+                vMoves.append(move)
+                validMoves.append([row,column])
+    print vMoves
+    return validMoves
 
 def isValidMove(row,column,player,board_position):
     directions =  [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
@@ -70,8 +72,53 @@ def read_Input_File(filename):
 
     return start_player,max_depth,initial_board_position
 
+def changeBoard(board,player,move):
+    newBoard = []
+    for row in board:
+        newRow = []
+        for column in row:
+            newRow.append(column)
+        newBoard.append(newRow)
+    newBoard[move[0]][move[1]] = player
+
+    directions =  [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
+    for dir in range(0,8):
+        x = move[0]
+        y = move[1]
+        validMove = 0
+        temp_positions = []
+        while True:
+            x += directions[dir][0]
+            y += directions[dir][1]
+            if x < 0 or x > 7 or y < 0 or y > 7 :
+                break
+            if newBoard[x][y] == '*':
+                break
+            if newBoard[x][y] != player:
+                temp_positions.append([x,y])
+                validMove =+ 1
+            if newBoard[x][y] == player :
+                if validMove > 0 :
+                    for temp_position in temp_positions :
+                        newBoard[temp_position[0]][temp_position[1]] = player
+                    break
+                else :
+                    break
+
+    return newBoard
+
 if __name__ == "__main__":
     start_player,max_depth,initial_board_position = read_Input_File("input.txt")
     othello = Othello(start_player,max_depth,initial_board_position)
-    print initial_board_position
-    find_valid_moves(start_player,initial_board_position)
+    #print initial_board_position
+    opposite_player = 'X'
+    if start_player == 'X':
+        opposite_player = 'O'
+    validMoves = find_valid_moves(start_player,initial_board_position)
+    #print validMoves
+    for move in validMoves :
+        #change the board
+        newBoard = changeBoard(initial_board_position,start_player,move)
+        nextValidMoves = find_valid_moves(opposite_player,newBoard)
+        #print nextValidMoves
+
