05b029adce3316a1ef355059dd76e61bb80ddb67
vishal13may <vishal13may@gmail.com>
2017-02-02 23:57:17 -0800

    Generated Tree

--- Alpha_Beta_Pruning/hw1cs561s2017.py
+++ Alpha_Beta_Pruning/hw1cs561s2017.py
@@ -27,8 +27,9 @@ class Othello:
         self.board_position = board_position
 
 class Node:
-    def __init__(self,name,type,alpha,beta,value,children):
+    def __init__(self,name,position,type,alpha,beta,value,children):
         self.name = name
+        self.position = position
         self.type = type
         self.alpha = alpha
         self.beta = beta
@@ -124,7 +125,7 @@ def changeBoard(board,player,move):
 
     return newBoard
 
-def find_next_moves(max_depth,board_position,validMoves,player):
+def find_next_moves(max_depth,board_position,player,max_min_counter,root):
 
     previous_player = 'X'
     if player == 'X':
@@ -136,22 +137,99 @@ def find_next_moves(max_depth,board_position,validMoves,player):
 
     if max_depth < 1 :
         # Call evaluation function
-        print "Evaluation" + str(validMoves)
-        for move in validMoves:
-            board = changeBoard(board_position,previous_player,move)
+        for child in root.children:
+            board = changeBoard(board_position,previous_player,child.position)
             value = find_move_value(board,start_player)
             print value
         return
-    if len(validMoves) == 0:
+
+    if root.name == "root" and max_min_counter == 2 :
+        nextValidMoves = find_valid_moves(player,board_position)
+        if len(nextValidMoves) == 0:
+            name = "Pass"
+            type = 'MAX'
+            if max_min_counter % 2 == 0 :
+                type = 'MIN'
+            value = 99999
+            if type == "MAX":
+                value = -99999
+            children = []
+            move = []
+            node = Node(name,move,type,-99999,99999,value,children)
+            root.children.append(node)
+        else:
+            for move in nextValidMoves :
+                name = dic[move[1]] + str(move[0]+1)
+                type = 'MAX'
+                if max_min_counter % 2 == 0 :
+                    type = 'MIN'
+                value = 99999
+                if type == "MAX":
+                    value = -99999
+                children = []
+                node = Node(name,move,type,-99999,99999,value,children)
+                root.children.append(node)
+
+        find_next_moves(max_depth-1,board_position,next_player,max_min_counter+1,root)
+
+    elif len(root.children) == 1 and root.children[0].name == "Pass":
         #print "pass"
         nextValidMoves = find_valid_moves(player,board_position)
-        find_next_moves(max_depth-1,board_position,nextValidMoves,next_player)
+        if len(nextValidMoves) == 0:
+            name = "Pass"
+            type = 'MAX'
+            if max_min_counter % 2 == 0 :
+                type = 'MIN'
+            value = 99999
+            if type == "MAX":
+                value = -99999
+            children = []
+            move = []
+            node = Node(name,move,type,-99999,99999,value,children)
+            root.children[0].childre.append(node)
+        else :
+            for move in nextValidMoves :
+                name = dic[move[1]] + str(move[0]+1)
+                type = 'MAX'
+                if max_min_counter % 2 == 0 :
+                    type = 'MIN'
+                value = 99999
+                if type == "MAX":
+                    value = -99999
+                children = []
+                node = Node(name,move,type,-99999,99999,value,children)
+                root.children[0].children.append(node)
+        find_next_moves(max_depth-1,board_position,next_player,max_min_counter+1,root.children[0])
     else :
-        for move in validMoves :
+        for child in root.children :
             #change the board
-            newBoard = changeBoard(board_position,previous_player,move)
+            newBoard = changeBoard(board_position,previous_player,child.position)
             nextValidMoves = find_valid_moves(player,newBoard)
-            find_next_moves(max_depth-1,newBoard,nextValidMoves,next_player)
+            if len(nextValidMoves) == 0:
+                name = "Pass"
+                type = 'MAX'
+                if max_min_counter % 2 == 0 :
+                    type = 'MIN'
+                value = 99999
+                if type == "MAX":
+                    value = -99999
+                children = []
+                move = []
+                node = Node(name,move,type,-99999,99999,value,children)
+                child.children.append(node)
+            else :
+                for move in nextValidMoves :
+                    name = dic[move[1]] + str(move[0]+1)
+                    type = 'MAX'
+                    if max_min_counter % 2 == 0 :
+                        type = 'MIN'
+                    value = 99999
+                    if type == "MAX":
+                        value = -99999
+                    children = []
+                    node = Node(name,move,type,-99999,99999,value,children)
+                    child.children.append(node)
+            find_next_moves(max_depth-1,newBoard,next_player,max_min_counter+1,child)
     return
 
 def find_move_value(board_position,start_player):
@@ -166,18 +244,38 @@ def find_move_value(board_position,start_player):
                 opposite_player_value += evaluation_values[row][column]
     return (start_player_value - opposite_player_value)
 
+def print_tree(root):
+
+    print "Name =" + root.name + " Alpha =" + str(root.alpha) + " Beta =" + str(root.beta) + " Position =" + str(root.position) + " Type =" + root.type
+
+    if len(root.children) == 0 :
+        return
+
+    for child in root.children :
+        print_tree(child)
+
+    return
+
 if __name__ == "__main__":
     start_player,max_depth,initial_board_position = read_Input_File("input.txt")
     othello = Othello(start_player,max_depth,initial_board_position)
-    #print initial_board_position
+
     opposite_player = 'X'
     if start_player == 'X':
         opposite_player = 'O'
 
-    validMoves = find_valid_moves(start_player,initial_board_position)
-    #print validMoves
-    
-    find_next_moves(max_depth - 1,initial_board_position,validMoves,opposite_player)
+    #validMoves = find_valid_moves(start_player,initial_board_position)
+
+    children = []
+    position = []
+
+    root = Node("root",position,"MAX",-99999,99999,-99999,children)
+
+    find_next_moves(max_depth,initial_board_position,start_player,2,root)
+
+    #print len(root.children)
+
+    print_tree(root)
 
     '''for move in validMoves :
         #change the board
